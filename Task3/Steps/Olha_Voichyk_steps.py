a555dacee610a8aee77e17bea0acbb6eec407501
Olha Voichyk <66120179+Riklia@users.noreply.github.com>
2022-11-16 23:55:51 +0100

    created Olha_Voichyk_steps.py

---
+++ Task3/Steps/Olha_Voichyk_steps.py
@@ -0,0 +1,120 @@
+from behave import *
+
+@given(u'we are at "https://www.epam.com/"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given we are at "https://www.epam.com/"')
+
+
+@when(u'we click on "Global (EN)"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we click on "Global (EN)"')
+
+
+@then(u'we get a list of avaliable languages')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we get a list of avaliable languages')
+
+
+@given(u'we are at "https://www.epam.com/about/who-we-are/contact"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given we are at "https://www.epam.com/about/who-we-are/contact"')
+
+
+@when(u'we leave required fields (with *) empty')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we leave required fields (with *) empty')
+
+
+@then(u'we get these fields red and with the corresponding message')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we get these fields red and with the corresponding message')
+
+
+@given(u'we are at "https://www.epam.com/" and click on the magnifier icon')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given we are at "https://www.epam.com/" and click on the magnifier icon')
+
+
+@when(u'we enter "Cloud" and click on the "Find"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we enter "Cloud" and click on the "Find"')
+
+
+@then(u'we are redirected on the results page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we are redirected on the results page')
+
+
+@when(u'we click on "HOW WE DO IT"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we click on "HOW WE DO IT"')
+
+
+@then(u'we get "https://www.epam.com/how-we-do-it" opened')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we get "https://www.epam.com/how-we-do-it" opened')
+
+
+@given(u'we are at "https://www.epam.com/careers"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given we are at "https://www.epam.com/careers"')
+
+
+@when(u'we choose location Kyiv and click on "Find"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we choose location Kyiv and click on "Find"')
+
+
+@then(u'we get list of job offers in Kyiv')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we get list of job offers in Kyiv')
+
+
+@given(u'we are at "https://www.epam.com/careers/job-listings?country=Ukraine&city=Kyiv"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given we are at "https://www.epam.com/careers/job-listings?country=Ukraine&city=Kyiv"')
+
+
+@when(u'we click on the "Home" label or "<epam>" icon')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we click on the "Home" label or "<epam>" icon')
+
+
+@then(u'we are redirected to "https://www.epam.com/"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we are redirected to "https://www.epam.com/"')
+
+
+@when(u'we click on the LinkedIn icon')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we click on the LinkedIn icon')
+
+
+@then(u'we have EPAM profile on LinkedIn')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we have EPAM profile on LinkedIn')
+
+
+@when(u'we click on the "+1-267-759-9000"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we click on the "+1-267-759-9000"')
+
+
+@then(u'we are asked to choose the device to send the phone number')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we are asked to choose the device to send the phone number')
+
+
+@given(u'we are at "https://www.epam.com/services/consult-and-design"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given we are at "https://www.epam.com/services/consult-and-design"')
+
+
+@when(u'we click on the play button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we click on the play button')
+
+
+@then(u'the video is playing')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the video is playing')
